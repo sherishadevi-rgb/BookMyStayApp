c24aff4e5afdad2c343399597b4eb08c5d1589f3
sherishadevi-rgb <sherishadevi@gmail.com>
2026-03-09 09:26:09 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,10 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to BookMyStay - Hotel Booking System");
+        System.out.println("Application Version: v1.0");
+        System.out.println("Application started successfully.");
+
+    }
+}
