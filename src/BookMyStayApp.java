6dcc4b32d6a9c2721a875cec544392e2820c697d
sherishadevi-rgb <sherishadevi@gmail.com>
2026-03-16 09:49:13 +0530

    UC5:Booking Request

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,20 +1,107 @@
+import java.util.HashMap;
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        // Application Name and Version
-        String appName = "BookMyStay - Hotel Booking System";
-        String version = "Version 1.0";
+        RoomInventory inventory = new RoomInventory();
 
-        // Welcome Message
-        System.out.println("=======================================");
-        System.out.println("        Welcome to " + appName);
-        System.out.println("               " + version);
-        System.out.println("=======================================");
-        System.out.println("Your easy solution for booking hotels.");
-        System.out.println("Application started successfully!");
+        Room[] rooms = {
+                new SingleRoom(),
+                new DoubleRoom(),
+                new SuiteRoom()
+        };
 
-        // Application ends
-        System.out.println("Thank you for using BookMyStay.");
+        SearchService searchService = new SearchService(inventory);
+
+        System.out.println("Welcome to BookMyStay - Room Search\n");
+
+        searchService.searchAvailableRooms(rooms);
+    }
+}
+
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected double size;
+    protected double price;
+
+    public Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sqm");
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 20, 50);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 30, 80);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 50, 150);
+    }
+}
+
+class RoomInventory {
+
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 0);
+    }
+
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+}
+
+class SearchService {
+
+    private RoomInventory inventory;
+
+    public SearchService(RoomInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public void searchAvailableRooms(Room[] rooms) {
+
+        for (Room room : rooms) {
+
+            int available = inventory.getAvailability(room.getRoomType());
+
+            if (available > 0) {
+                room.displayDetails();
+                System.out.println("Available Rooms: " + available);
+                System.out.println();
+            }
+        }
     }
 }
