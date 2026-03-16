1ab4b4ca0a7e5b0ab5fbd8d494f37df40dd53cc4
sherishadevi-rgb <sherishadevi@gmail.com>
2026-03-16 09:59:18 +0530

    UC9:Error Handling & Validation

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,20 +1,63 @@
+import java.util.*;
+
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+class BookingHistory {
+    private List<Reservation> confirmedReservations;
+
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+
+class BookingReportService {
+
+    public void generateReport(BookingHistory history) {
+        System.out.println("\nBooking History Report");
+
+        for (Reservation r : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + r.getGuestName() + ", Room Type: " + r.getRoomType());
+        }
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        // Application Name and Version
-        String appName = "BookMyStay - Hotel Booking System";
-        String version = "Version 1.0";
+        System.out.println("Booking History and Reporting");
 
-        // Welcome Message
-        System.out.println("=======================================");
-        System.out.println("        Welcome to " + appName);
-        System.out.println("               " + version);
-        System.out.println("=======================================");
-        System.out.println("Your easy solution for booking hotels.");
-        System.out.println("Application started successfully!");
+        BookingHistory history = new BookingHistory();
 
-        // Application ends
-        System.out.println("Thank you for using BookMyStay.");
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        BookingReportService reportService = new BookingReportService();
+        reportService.generateReport(history);
     }
 }
